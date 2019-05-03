bb290d09d2318fb4e5e86a1db96613536ab1157c
WMXPY <wm@mengw.io>
2019-05-02 22:39:57 -0500

    add express handler

--- src/service.ts
+++ src/service.ts
@@ -4,7 +4,28 @@
  * @description Service
  */
 
-export const createKunnServer = (): any => {
+import * as BodyParser from "body-parser";
+import * as Express from "express";
 
-    return;
+export const createKunnServer = (): Express.Express => {
+
+    const app: Express.Express = Express();
+
+    app.use(BodyParser.json());
+    app.use(BodyParser.urlencoded({
+        extended: true,
+    }));
+
+    app.use((_: Express.Request, res: Express.Response, next: Express.NextFunction) => {
+
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+        res.header("X-Powered-By", "Kunn");
+
+        next();
+    });
+
+    return app;
 };
